5b2ec267fbf897f22994f38c15b97bef7e1053f4
AlexShkarin <shkarinalexander278@gmail.com>
2020-03-12 08:58:59 -0400

    chore: added button click function to api.js

--- quotesTheme/js/api.js
+++ quotesTheme/js/api.js
@@ -1,17 +1,26 @@
 (function($) {
   console.log(red_vars);
   console.log(red_vars.rest_url);
+  console.log(
+    red_vars.rest_url +
+      'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=10'
+  );
+
   $('.randomQuote').on('click', function(event) {
+    console.log('Enter to function...');
     $.ajax({
-      method: 'GET',
-      url:
-        red_vars.rest_url +
-        'wp/v2/posts/?filter[orderby]=rand&filter[posts_per_page]=10'
-    }).done(function(data) {
-      console.log('123...');
-      console.log(data);
-      console.log('Get a date - ', data[0].date);
-      //   alert('Success! Comments are closed for this post.');
-    });
+      type: 'GET',
+      url: red_vars.rest_url
+      // + '/wp/v2/posts'
+      'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=10'
+    })
+      .done(function(data) {
+        console.log('Ajax done...');
+        // console.log(data);
+      })
+      .fail(function(data) {
+        console.log('Ajax fail...');
+      });
+    console.log('Finish...');
   });
 })(jQuery);
